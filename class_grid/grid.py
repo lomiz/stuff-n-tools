a4bbf1b0161b9af1519056da838b358cebcf9a05
Enrico lomiz Zimol <lomiz.mail@gmail.com>
2014-09-23 18:00:56 +0200

    Grid class, completed is_out_of_bounds() and get_neighbours..

--- class_grid/grid.py
+++ class_grid/grid.py
@@ -42,6 +42,12 @@ class Grid:
         #     - che i valori delle coordinate siano numberi naturali)
         #     - spazio (deve essere dentro le coordinate massime)
         #     - che gli elementi nel dizionario elements siano in elements_table
+
+        # checking out_of_bounds
+        for coordinates in elements:
+            if Grid.is_out_of_bounds(coordinates, grid_size):
+                raise CoordinatesError("Coordinates %s of the element %s are out of bounds." % (coordinates, elements[coordinates]))
+                #raise CoordinatesError("prova")
         self.elements = dict(elements)
         self.space = self.elements
         self.grid_size = grid_size
@@ -55,14 +61,47 @@ class Grid:
         return r
 
     def is_empty(self, coordinates):
-        if tuple(coordinates) in self.space:
-            return True
+        """
+        Check if there is an element at given coordinates (no element definition in space dictionary)
+        """
+        if tuple(coordinates) in self.space:  # if in space dictionary there is not a definition "coordinates: elements"
+            return False                      # means it must be an empty position (void filled)
         else:
-            return False
+            return True
 
-    # TODO:
-    # Numero di iterazioni corretto (sembra)
-    # verificare i risultati delle coordinate
+    @staticmethod
+    def is_out_of_bounds(coordinates, grid_size):
+        """
+        Check if given coordinates are out of grid bounds:
+         - all coordinates must be positive because Grid use top-right part of a cartesian system
+         - all coordinates must be inside the given grid_size
+        :param coordinates: coordinates tuple to check or list of them.
+                            If list, this method will return True just for a single tuple out of bounds.
+        :param grid_size: tuple containing the grid size that rappresent top limits of the coordinates
+
+        """
+        x_limit = (tuple(grid_size))[0]
+        y_limit = (tuple(grid_size))[1]
+        z_limit = (tuple(grid_size))[2]
+
+        if isinstance(coordinates, list):  # If user pass a list of tuple of coordinates i check everyone and return
+                                           # True if only one of them is wrong
+            for coordinates_tuple in coordinates:
+                x = int((tuple(coordinates_tuple))[0])  # ensure that list elements are or can be coverted in tuple
+                y = int((tuple(coordinates_tuple))[1])  # .. and convert the tuple elements in integers
+                z = int((tuple(coordinates_tuple))[2])  #
+                if (x <= 0 or y <= 0 or z <= 0) or (x > x_limit or y > y_limit or z > z_limit):
+                    return True
+
+        else:
+            x = int((tuple(coordinates))[0])  # ensure tuple composed by integers
+            y = int((tuple(coordinates))[1])  # .. same
+            z = int((tuple(coordinates))[2])  # .. same
+            if (x <= 0 or y <= 0 or z <= 0) or (x > x_limit or y > y_limit or z > z_limit):
+                return True
+
+        return False
+
     @staticmethod
     def get_neighbours_coordinates(coordinates, neighbours_cube_range=3):
         """
@@ -88,6 +127,7 @@ class Grid:
         neighbours = []
         x = coordinates[0]
         y = coordinates[1]
+        z = coordinates[2]
 
         # Depth layer variation from center of the neighbours cube
         # 3 lenght side cube: -((3-1)/2) --> -1
@@ -97,8 +137,8 @@ class Grid:
         # z variation
         layer_variator = variator
 
-        print "variator %s" % variator
-        print "variator_limit %s" % variator_limit
+        #print "variator %s" % variator  ====================FOR DEBUGGING====================
+        #print "variator_limit %s" % variator_limit  ====================FOR DEBUGGING====================
 
         # Image a 3x3 cube saw frontally where the element coordinates given are the center
         # Every while cicle we change layer:
@@ -106,7 +146,7 @@ class Grid:
         # - second cycle will be the same layer of the element coordinates given
         # - third cycle will be the bottom layer
         while layer_variator < variator_limit:
-            layer = coordinates[2]+layer_variator
+            layer = z+layer_variator
             y_variator = variator
 
             while y_variator < variator_limit:
@@ -115,7 +155,21 @@ class Grid:
 
                 while x_variator < variator_limit:
                     column = x+x_variator
-                    neighbours.append((column, row, layer))
+                    tmp_grid_size = neighbours_cube_range  # is_out_of_bounds() required a tuple (x,y,z) style for the
+                                                           # grid_size parameter to know what are the grid limits
+
+                    if not Grid.is_out_of_bounds((column, row, layer), (tmp_grid_size, tmp_grid_size, tmp_grid_size)):
+                        # now i'm sure that current coordinates are not out of bounds. A coordinate is out_of_bounds if
+                        # it's negative or if it's major than neighbours_cube_range.
+                        # NB: As implemented there should not be coordinates major than neighbours_cube_range:
+                        #    we use is_out_of_bounds() for preventing negative coordinates to be added.
+                        if not (column == coordinates[0] and row == coordinates[1] and layer == coordinates[2]):
+                            # ensure to return neighbours only (not coords themself)
+                            neighbours.append((column, row, layer))
+                        # else: ====================FOR DEBUGGING====================
+                        #    print "%s sono le stesse coordinate passate" % str((column, row, layer))
+                    # else: ====================FOR DEBUGGING====================
+                    #    print "%s e' fuori dai limiti" % str((column, row, layer))
                     x_variator += 1
 
                 y_variator += 1
@@ -125,11 +179,36 @@ class Grid:
         return neighbours
 
 
-elementi = {(2, 2, 2): 'c', (1, 1, 1): 'a', (1, 1, 3): 'a', (3, 3, 1): 'a', (3, 3, 3): 'a'}
-g = Grid(elementi, grid_size=(3, 3, 3))
-
+elementi = {(2, 1, 2): 'c', (1, 1, 1): 'a', (1, 1, 3): 'a', (3, 1, 1): 'a', (3, 1, 3): 'a'}
+try:
+    g = Grid(elementi, grid_size=(3, 3, 2))
+except e as CoordinatesError:
+    print e
+    quit()
 print g
 
-c = g.get_neighbours_coordinates((3, 3, 3), 9)
+c = g.get_neighbours_coordinates((1, 1, 1), 3)
 print c
 print len(c)
+
+
+
+
+cordlist = []
+for tupla in elementi:
+    cordlist.append(tupla)
+
+print cordlist
+if g.is_out_of_bounds(cordlist, (3, 1, 3)):
+    print "ci sono velori non validi"
+
+# Prove per scartare le coordinate fuori piano cartesiano
+"""d = []
+for elem in c:
+    if not (elem[0] <= 0 or elem[1] <= 0 or elem[2] <= 0):
+        d.append(elem)
+
+
+print d
+print len(d)
+"""
